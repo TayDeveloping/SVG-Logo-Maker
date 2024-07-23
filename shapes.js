0b25d6ca80b413984067c490a30c0c7492bfc5bb
tay <renegademisfit99@gmail.com>
2024-07-22 21:55:28 -0400

    added shapes test

--- shapes.js
+++ shapes.js
@@ -28,30 +28,31 @@ const questions = [
 ];
 
 inquirer.prompt(questions).then((answers) => {
-    let shape;
-  
-    switch (answers.shape) {
-      case 'Triangle':
-        shape = new Triangle();
-        break;
-      case 'Circle':
-        shape = new Circle();
-        break;
-      case 'Square':
-        shape = new Square();
-        break;
-    }
-  
-    shape.setColor(answers.shapeColor);
-  
-    const svgContent = `
-      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        ${shape.render()}
-        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
-      </svg>
-    `;
-  
-    fs.writeFileSync('logo.svg', svgContent.trim());
-    console.log('Generated logo.svg');
-  });
+  let shape;
+
+  switch (answers.shape) {
+    case 'Triangle':
+      shape = new Triangle();
+      break;
+    case 'Circle':
+      shape = new Circle();
+      break;
+    case 'Square':
+      shape = new Square();
+      break;
+  }
+
+  shape.setColor(answers.shapeColor);
+
+  const svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${shape.render()}
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+    </svg>
+  `;
+
+  fs.writeFileSync('logo.svg', svgContent.trim());
+  console.log('Generated logo.svg');
+});
+
   
